4dd264e3d8610352d5ed10c0f0056cea80b4ccd1
achmadzahrul01 <achmadzahrul01@gmail.com>
2024-11-15 14:56:03 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-peachred">Hello, React!</h1>
+      <p>Hello, React!</p>
+      <p className='font-raleway'>Hello, React!</p>
+    </div>
+  );
+};
+
+export default App;
